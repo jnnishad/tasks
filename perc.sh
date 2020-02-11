6fc6601357ba933bdb9a21f6fb36f393bdd1abf2
jnnishad <50562744+jnnishad@users.noreply.github.com>
2020-02-11 09:33:08 +0530

    Update perc.sh

--- perc.sh
+++ perc.sh
@@ -1,48 +1,46 @@
 #!/bin/bash
 
-#pid=$1
-#tag[0]="\\"
-#tag[1]="-"
-#tag[2]="|"
-#tag[3]="_"
-#tag[4]="/"
-##while true
-##do
-##len=${#tag}
-##do
-##echo -ne "$tag[$i]"\b
-##done
-#echo -ne "working ${tag[0]}"
-#while true # [[ `kill -0 $pid` ]]
-#do
-#for i in "${tag[@]}"
-#do
-#for ((j=1;j<=10;j++))
-#do
-# echo -ne "`tput setaf 2`\b.$i`tput sgr0`"
-#
-#        sleep  0.2
-#done
-#for ((j=10;j>=1;j--))
-#do
-# echo -ne "`tput setaf 2`\b`tput sgr0`"
-#
-#        sleep  0.2
-#done
-#done
-#done
+pid=$1
+tag[0]="\\"
+tag[1]="-"
+tag[2]="|"
+tag[3]="_"
+tag[4]="/"
+while true
+do
+len=${#tag}
+do
+echo -ne "$tag[$i]"\b
+done
+echo -ne "working ${tag[0]}"
+while true # [[ `kill -0 $pid` ]]
+do
+for i in "${tag[@]}"
+do
+for ((j=1;j<=10;j++))
+do
+ echo -ne "`tput setaf 2`\b.$i`tput sgr0`"
+        sleep  0.2
+done
+for ((j=10;j>=1;j--))
+do
+ echo -ne "`tput setaf 2`\b`tput sgr0`"
 
-#while true # [[ `kill -0 $pid` ]]
-#do
-#for i in "${tag[@]}"
-#do
-#	echo -ne "`tput setaf 2`\b.$i`tput sgr0`"
-#
-#	sleep  0.2
-#done 
-#done
-#echo ''
-##done
+        sleep  0.2
+done
+done
+done
+while true # [[ `kill -0 $pid` ]]
+do
+for i in "${tag[@]}"
+do
+	echo -ne "`tput setaf 2`\b.$i`tput sgr0`"
+
+	sleep  0.2
+done 
+done
+echo ''
+done
 
 
 pattern='#'
