48ceb55a8aa06d295f62a26d29d37ff7dab2d358
jnnishad <50562744+jnnishad@users.noreply.github.com>
2020-02-11 09:30:32 +0530

    Update re.py

--- re.py
+++ re.py
@@ -1,27 +1,22 @@
 #!/bin/python
-#import re
-#import glob
-#
-#
-#
-#
-#for line in content :
-#	for file in glob.iglob(line):
-#		for arg in open("re_test_arena","r")
-#	if re.search("Paynetz", content):
-#		print line,
-#content.close()
+import re
+import glob
 
+for line in content :
+	for file in glob.iglob(line):
+		for arg in open("re_test_arena","r")
+	if re.search("Paynetz", content):
+		print line,
+content.close()
 #Paynetz transaction id created as
-
 import re
 import os
 import sys
 
-#for f in filter(os.path.isfile, sys.argv[2:]):
-#	for line in open(f).readline():
-#		if re.findall(sys.argv[1], line):
-#			print line
+for f in filter(os.path.isfile, sys.argv[2:]):
+	for line in open(f).readline():
+		if re.findall(sys.argv[1], line):
+			print line
 
 file = open("re_test_arena","r")
 for line in file:
