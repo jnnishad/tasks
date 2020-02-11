3b523929b7debb3713ad0d41f0ba31cd435fea39
jnnishad <50562744+jnnishad@users.noreply.github.com>
2020-02-11 09:30:48 +0530

    Delete fetchline.sh

--- fetchline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-txn=225905024
-file=server.log.2018-11-23
-top=`cat -n $file| grep $txn|head -1|awk '{print$1}'`
-bot=`cat -n $file| grep $txn|tail -1|awk '{print$1}'`
-#echo -e "$top - $bot"
-
-sed -n  "$top,$bot p" $file
