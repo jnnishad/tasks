e98c4f350e24b44d231d59d6ca23d938c6ebdd81
jnnishad <50562744+jnnishad@users.noreply.github.com>
2020-07-07 21:06:31 +0530

    Delete sat.sh

--- sat.sh
+++
@@ -1,7 +0,0 @@
-  File: `.system/crpyt'
-  Size: 21        	Blocks: 8          IO Block: 4096   regular file
-Device: 803h/2051d	Inode: 1442268     Links: 1
-Access: (0700/-rwx------)  Uid: (  505/     jai)   Gid: (  505/     jai)
-Access: 2018-12-24 10:49:18.596788847 +0530
-Modify: 2018-12-21 15:21:10.214831196 +0530
-Change: 2018-12-21 15:21:10.244831194 +0530
